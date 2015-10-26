d7486a74772fa5e764cd06df35e23ba063487929
chenpengpeng <cpp@changingedu.com>
2015-10-26 18:08:50 +0800

    add new app

---
+++ requirement/models.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+import sys
+from django.db import models
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+# Create your models here.
+class Requirement(models.Model):
+    enroll_date = models.DateTimeField("录入时间")
+    pd_owner = models.CharField("产品负责人", max_length=20)
+    business_type = models.CharField("业务类型", max_length=100)
+    requirement_desc = models.TextField('需求描述')
+    solution = models.TextField('解决方案')
+    estimate_solute_date = models.DateField('预计解决时间')
+    actual_solute_date = models.DateField('实际解决时间')
+    requirement_type = models.CharField('需求类型', max_length=100)
+    requirement_source = models.CharField('需求来源', max_length=100)
+    requirement_status = models.CharField('需求状态', max_length=100)
